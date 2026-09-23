633a542f0e381a249fe6c3e38315947078667a38
Phillbo-vs <phillborsonelli@gmail.com>
2026-09-23 03:17:58 +0100

    Remove TODO comments from Caesar Cipher

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -1,10 +1,6 @@
-# TODO-1: Import and print the logo from art.py when the program starts.
 from art import logo
 print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-# TODO-2: What happens if the user enters a number/symbol/space?
-
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
@@ -20,7 +16,6 @@ def caesar(original_text, shift_amount, encode_or_decode):
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 keep_running = True
-# TODO-3: Can you figure out a way to restart the cipher program?
 
 while keep_running:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
@@ -30,5 +25,3 @@ while keep_running:
     if input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower() == "no":
         keep_running = False
         print("Goodbye!")
-
-
