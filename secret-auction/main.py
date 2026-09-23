1ec3aa8ebb1ce0e0d3f4a20f4eb7277d72b068fd
Phillbo-vs <phillborsonelli@gmail.com>
2026-09-23 03:05:36 +0100

    Add Secret Auction project

---
+++ secret-auction/main.py
@@ -0,0 +1,29 @@
+# TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+
+from art import logo
+print(logo)
+
+keep_running = True
+bid_board = {}
+
+while keep_running:
+    name = input("What is your name?")
+    bid = int(input("What is your bid?"))
+    bid_board[name] = bid
+    if input('Are there any other bidders? Type "yes" or "no"\n').lower() == "no":
+        keep_running = False
+    else:
+        print("\n" * 1000)
+
+highest_bid = 0
+winner = ""
+
+for bidder in bid_board:
+    if bid_board[bidder] > highest_bid:
+        highest_bid = bid_board[bidder]
+        winner = bidder
+
+print(f"The winner is {winner} with a bid of ${highest_bid}")
